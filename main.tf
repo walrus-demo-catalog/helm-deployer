d7d6c6813b2e2100e6e94b1f1749789bb44c2a3b
Yinlin Li <yinlin@seal.io>
2024-01-09 17:42:31 +0800

    fix: providers version

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.25.1"
+      version = ">= 2.23.0"
     }
-
     helm = {
       source  = "hashicorp/helm"
-      version = "2.12.1"
+      version = ">= 2.11.0"
     }
   }
 }
